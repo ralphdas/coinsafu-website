e51038da6550cde57388d70d6a058907efb4a8db
Das, Ralph <ralph.das@klm.com>
2020-08-18 09:37:37 +0300

    feature(add pages): various

--- src/main.js
+++ src/main.js
@@ -2,17 +2,35 @@ import './scss/main.scss';
 // templates
 import './templates/index.pug';
 import './templates/buy.pug';
-import './templates/buy.pug';
+import './templates/faq.pug';
 
 import axios from 'axios';
+import { lory } from 'lory.js';
 
 const modalContainerElem = document.querySelector('.csu__modal-container');
 const modalCloseElem = document.querySelector('.csu__modal-dialog-close');
 const formElem = document.querySelector('.csu__submit-form');
 const inputElem = document.querySelector('.csu__email-input');
+const sliderElem = document.querySelector('.js_slider');
 
-modalCloseElem.addEventListener('click', closeModal);
-formElem.addEventListener('submit', onSubmitHandler);
+document.addEventListener('DOMContentLoaded', onInit);
+
+function onInit() {
+  if (modalCloseElem) {
+    modalCloseElem.addEventListener('click', closeModal);
+  }
+
+  if (formElem) {
+    formElem.addEventListener('submit', onSubmitHandler);
+  }
+
+  if (sliderElem) {
+    lory(sliderElem, {
+      infinite: 1,
+      enableMouseEvents: true,
+    });
+  }
+}
 
 function onSubmitHandler(event) {
   event.preventDefault();
